aab0f8dabbb64814047c852f2eb21bb6bcd445ee
Ahok <2579585529@qq.com>
2022-06-23 20:48:45 +0800

    suspense

--- src/views/Outlook.tsx
+++ src/views/Outlook.tsx
@@ -1,63 +1,118 @@
-import { Text, Group, NumberInput, Portal } from "@mantine/core"
-import { Fragment, ReactNode, useCallback, useState } from "react"
+import { Group, List, Title } from "@mantine/core"
+import { Suspense } from "react"
 
-function fahrenheitToCelsius(fahrenheit: number) {
-  return fahrenheit ? (fahrenheit - 32) / 1.8 : 0
-}
-
-function celsiusToFahrenheit(celsius: number) {
-  return celsius ? celsius + 32 * 1.8 : 0
-}
-
-function Thermometer() {
-  const [celsius, setCelsius] = useState(0)
-  const [fahrenheit, setFahrenheit] = useState(0)
-
-  const onCelsius = useCallback(
-    (value: number) => {
-      setCelsius(value)
-      setFahrenheit(celsiusToFahrenheit(value))
-    },
-    [celsius]
-  )
-
-  const onFahrenheit = useCallback(
-    (value: number) => {
-      setFahrenheit(value)
-      setCelsius(fahrenheitToCelsius(value))
-    },
-    [fahrenheit]
-  )
-
-  return (
-    <Group>
-      <NumberInput hideControls label="Celsius" placeholder="Input celsius" value={celsius} onChange={onCelsius} />
-      <NumberInput
-        hideControls
-        label="Fahrenheit"
-        placeholder="Input fahrenheit"
-        value={fahrenheit}
-        onChange={onFahrenheit}
-      />
-    </Group>
-  )
-}
-
-function ChilrenDemo({ children }: { children(props: { name: string }): ReactNode }) {
-  return <div>{children({ name: "Leanne Graham" })}</div>
-}
+const resource = fetchProfileData()
 
 export default function Outlook() {
   return (
-    <Group direction="column">
-      <Thermometer />
-      <ChilrenDemo>
-        {({ name }) => (
-          <Text color="cyan">
-            Hello <strong>{name}</strong>
-          </Text>
-        )}
-      </ChilrenDemo>
-    </Group>
+    <Suspense fallback={<Title order={2}>Loading profile...</Title>}>
+      <Group direction="column" spacing="sm">
+        <ProfileUser />
+        <Suspense fallback={<Title order={3}>Loading todos...</Title>}>
+          <ProfileTodos />
+        </Suspense>
+      </Group>
+    </Suspense>
   )
+}
+
+function ProfileUser() {
+  const user = resource.user.read()
+  return <Title order={2}>{user.name}</Title>
+}
+
+function ProfileTodos() {
+  const todos = resource.todos.read()
+  return (
+    <List withPadding>
+      {todos.map((todo: { id: number; title: string }) => (
+        <List.Item key={todo.id}>{todo.title}</List.Item>
+      ))}
+    </List>
+  )
+}
+
+function fetchProfileData() {
+  const userPromise = fetchUser()
+  const todosPromise = fetchTodos()
+  return {
+    user: wrapPromise(userPromise),
+    todos: wrapPromise(todosPromise)
+  }
+}
+
+enum Status {
+  PENDING = "pending",
+  SUCCESS = "success",
+  ERROR = "error"
+}
+
+function wrapPromise(promise: Promise<any>) {
+  let status = Status.PENDING
+  let result: any = null
+
+  const suspender = promise
+    .then(data => {
+      status = Status.SUCCESS
+      result = data
+    })
+    .catch(err => {
+      status = Status.ERROR
+      result = err
+    })
+
+  return {
+    read() {
+      if (status === Status.PENDING) {
+        throw suspender
+      } else if (status === Status.SUCCESS) {
+        return result
+      } else {
+        throw result
+      }
+    }
+  }
+}
+
+function fetchUser() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        name: "Leanne Graham"
+      })
+    }, 1000)
+  })
+}
+
+function fetchTodos() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve([
+        {
+          id: 1,
+          title: "suscipit repellat esse quibusdam voluptatem incidunt"
+        },
+        {
+          id: 2,
+          title: "distinctio vitae autem nihil ut molestias quo"
+        },
+        {
+          id: 3,
+          title: "et itaque necessitatibus maxime molestiae qui quas velit"
+        },
+        {
+          id: 4,
+          title: "adipisci non ad dicta qui amet quaerat doloribus ea"
+        },
+        {
+          id: 5,
+          title: "voluptas quo tenetur perspiciatis explicabo natus"
+        },
+        {
+          id: 6,
+          title: "aliquam aut quasi"
+        }
+      ])
+    }, 2000)
+  })
 }
