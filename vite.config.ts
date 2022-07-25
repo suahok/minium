4471ef8abb83e24e27e2a9fbd669d1731f40142b
Ahok <2579585529@qq.com>
2022-07-25 16:23:28 +0800

    配置构建输出的<manualChunks>

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { resolve } from "path"
-import { defineConfig, splitVendorChunkPlugin } from "vite"
+import { defineConfig } from "vite"
 import viteReact from "@vitejs/plugin-react"
 
 export default defineConfig({
-  plugins: [viteReact(), splitVendorChunkPlugin()],
+  plugins: [viteReact()],
   resolve: {
     alias: {
       "@": resolve(__dirname, "src")
@@ -14,19 +14,19 @@ export default defineConfig({
     rollupOptions: {
       external: [""],
       output: {
-        // manualChunks(chunkId) {
-        //   if (chunkId.includes("node_modules")) {
-        //     return chunkId.split("node_modules/")[1].split("/")[0].toString()
-        //   }
-        // },
-        entryFileNames: "scripts/[name].[hash].js",
-        chunkFileNames: "scripts/[name].[hash].js",
+        manualChunks(chunkId) {
+          if (chunkId.includes("node_modules")) {
+            return chunkId.split("node_modules/")[1].split("/")[0].toString()
+          }
+        },
+        entryFileNames: "script/[name].[hash].js",
+        chunkFileNames: "script/[name].[hash].js",
         assetFileNames({ name }) {
           const extname = name?.toString().split(".")[1]
           if (extname === "css") {
             return "styles/[name].[hash].[ext]"
           } else {
-            return "[ext]/[name].[hash].[ext]"
+            return "assets/[ext]/[name].[hash].[ext]"
           }
         }
       }
